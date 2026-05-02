9a9be456b3ac4e73955af33707204a668a4e3c81
Bernardo Vieira <bvieira.lists@gmail.com>
2026-05-02 07:42:39 -0300

    Port some basic options from nvim-kickstart

--- lua/common/globals.lua
+++ lua/common/globals.lua
@@ -1,8 +1,8 @@
--- set leader to <Space>
-vim.g.mapleader = " "
-
--- set local leader to \\
-vim.g.maplocalleader = "\\"
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 
 vim.g.netrw_banner = 0        -- Hide the help banner at the top
--- lua/common/set.lua
+++ lua/common/set.lua
@@ -1,3 +1,14 @@
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+-- Enable break indent
+vim.o.breakindent = true
+
+
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
@@ -12,26 +23,66 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+
+-- Disable swapfile and backups
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+-- Enable undo/redo changes even after closing and reopening a file
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = true -- Highlight search hits
-vim.opt.incsearch = true -- Incremental search
-vim.opt.ignorecase = true -- Ignore case in search
-vim.opt.smartcase = true  -- Smart case sensitivity
+-- Search behavior
+-- Highlight search hits
+vim.opt.hlsearch = true
+-- Incremental search
+vim.opt.incsearch = true 
+-- Ignore case in search
+vim.opt.ignorecase = true 
+-- Smart case sensitivity
+vim.opt.smartcase = true  
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8 -- scroll context lines
-vim.opt.signcolumn = "yes" -- always reserve column for LSP signs
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
 vim.opt.isfname:append("@-@") -- fixes gf for files with "@" in filename
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 250
 
 vim.opt.colorcolumn = "80"
 
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
 
-vim.opt.splitright = true   -- Open new vertical splits to the right
-vim.opt.splitbelow = true   -- Open new horizontal splits below
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-guide-options`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
