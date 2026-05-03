1606f091c786a9cb0cca9cd53850e2d9ef1a067c
Bernardo Vieira <bvieira.lists@gmail.com>
2026-05-03 20:18:10 -0300

    Add handy remaps

--- lua/common/keys.lua
+++ lua/common/keys.lua
@@ -14,7 +14,7 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight'})
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search '})
 
 
 -- Diagnostic Config & Keymaps
@@ -35,3 +35,19 @@ vim.diagnostic.config {
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = '[p]aste over selection, preserve register' })
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = '[y]ank into system clipboard'})
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = '[Y]ank into system clipboard'})
+
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d", { desc = '[d]elete, preserve register'})
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = 'Make file e[x]ecutable' })
+vim.keymap.set("n", "J", "mzJ`z", { desc = 'Append next line into current'})
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Jump 1/2 page [d]own'})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Jump 1/2 page [u]p'})
+vim.keymap.set("n", "n", "nzzzv", { desc = '[n]ext search hit'})
+vim.keymap.set("n", "N", "Nzzzv", { desc = '[N]ext search hit'})
