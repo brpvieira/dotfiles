f43ca0a606f246d0be71d9f01de469cff4f369a8
Bernardo Vieira <bvieira.lists@gmail.com>
2026-05-03 20:40:29 -0300

    Format file

--- lua/common/autos.lua
+++ lua/common/autos.lua
@@ -2,21 +2,21 @@
 --  See `:help lua-guide-autocommands`
 
 local augroup = vim.api.nvim_create_augroup
-local group = augroup('BootstrapGroup', { clear = true })
+local group = augroup("BootstrapGroup", { clear = true })
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = group,
-  callback = function()
-    vim.hl.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = group,
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
-vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  desc = 'Clear trailling whitespace on write',
-  group = group,
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	desc = "Clear trailling whitespace on write",
+	group = group,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
