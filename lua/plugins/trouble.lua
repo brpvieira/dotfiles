0fb7dd25556bb161344dc079cb352ba74bbad2c7
Bernardo Vieira <bvieira.lists@gmail.com>
2026-05-04 08:10:21 -0300

    Add minimal trouble plugin config

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,40 @@
+return {
+  "folke/trouble.nvim",
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>tx",
+      function ()
+        require("trouble").toggle("diagnostics")
+      end,
+      desc = "Diagnostics (Trouble)",
+    },
+--[[
+    {
+      "<leader>tX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>ts",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>tl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>tL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>tQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+]]--
+  },
+}
