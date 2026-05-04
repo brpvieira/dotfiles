1ed96517e4735006c4f3102e369b1c4d7f78e641
Bernardo Vieira <brpvieira@gmail.com>
2026-05-04 19:46:54 -0300

    Add todo comments keybindings to interact with Trouble

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,9 +1,20 @@
 return {
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    ---@module 'todo-comments'
-    ---@type TodoOptions
-    ---@diagnostic disable-next-line: missing-fields
-    opts = { signs = false },
+	"folke/todo-comments.nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local opts = { signs = true }
+		require("todo-comments").setup(opts)
+		vim.keymap.set("n", "]t", function()
+			require("todo-comments").jump_next()
+		end, { desc = "Next todo comment" })
+
+		vim.keymap.set("n", "[t", function()
+			require("todo-comments").jump_prev()
+		end, { desc = "Previous todo comment" })
+
+		vim.keymap.set("n", "<leader>tt", function()
+            require("trouble").toggle("todo")
+		end, { desc = "[T]odos (Trouble)" })
+	end,
 }
