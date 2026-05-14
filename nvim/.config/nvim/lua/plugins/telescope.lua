33939fa6ce48131eae88724a70247b5865d2f9db
Bernardo Vieira <brpvieira@gmail.com>
2026-05-14 16:28:54 -0300

    Enable telescope grep arguments extension

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -25,6 +25,12 @@ return {
             -- installed and loaded.
             cond = function() return vim.fn.executable 'make' == 1 end,
         },
+        {
+            "nvim-telescope/telescope-live-grep-args.nvim" ,
+            -- This will not install any breaking changes.
+            -- For major updates, this must be adjusted manually.
+            version = "^1.0.0",
+        },
         { 'nvim-telescope/telescope-ui-select.nvim' },
 
         -- Useful for getting pretty icons, but requires a Nerd Font.
@@ -52,24 +58,40 @@ return {
 
         -- [[ Configure Telescope ]]
         -- See `:help telescope` and `:help telescope.setup()`
-        require('telescope').setup {
+
+        local telescope = require('telescope')
+        local lga_actions = require("telescope-live-grep-args.actions")
+
+        telescope.setup {
             -- You can put your default mappings / updates / etc. in here
             --  All the info you're looking for is in `:help telescope.setup()`
             --
-            -- defaults = {
-            --   mappings = {
-            --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-            --   },
-            -- },
+            defaults = {
+              mappings = {
+                i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+              },
+            },
             -- pickers = {}
             extensions = {
                 ['ui-select'] = { require('telescope.themes').get_dropdown() },
+                ['live_grep_args'] = {
+                    auto_quoting = true,
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = lga_actions.quote_prompt(),
+                            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                            -- freeze the current list and start a fuzzy search in the frozen list
+                            ["<C-space>"] = lga_actions.to_fuzzy_refine,
+                        }
+                    }
+                }
             },
         }
 
         -- Enable Telescope extensions if they are installed
-        pcall(require('telescope').load_extension, 'fzf')
-        pcall(require('telescope').load_extension, 'ui-select')
+        pcall(telescope.load_extension, 'fzf')
+        pcall(telescope.load_extension, 'ui-select')
+        pcall(telescope.load_extension, 'live_grep_args')
 
         -- See `:help telescope.builtin`
         local builtin = require 'telescope.builtin'
@@ -79,6 +101,7 @@ return {
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sga', telescope.extensions.live_grep_args.live_grep_args, { desc = '[S]earch by [G]rep with [A]rgs' })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
